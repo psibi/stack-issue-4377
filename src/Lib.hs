b4e0f77ebf9e697084d93c2161b734c155b725e9
Sibi Prabakaran <sibi@psibi.in>
2018-12-10 18:17:08 +0530

    Make example more complete

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
     ( someFunc
     ) where
+
 import Data.Validity
+
+newtype Prime = Prime Int
+
+instance Validity Prime where
+    validate (Prime n) = check (n == 3) "Is it 3"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  let xs = validate (Prime 3)
+  print $ show xs
+  return ()
